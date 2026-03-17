d0e1cfd9a6b0e9a3d1c91896cf66efff968c9cfb
Rajat <rajat804@gmail.com>
2026-03-17 17:37:02 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ export default function Footer() {
           >
             <div className="flex items-center gap-2">
               <img
-                src={assets.Logo}
+                src={assets.GoldenStep}
                 alt="Logo"
                 className="w-80  rounded"
               />
