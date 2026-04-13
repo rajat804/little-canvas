c8710b1124ef3c43c487a26dec7170ed28b7979f
Rajat <rajat804@gmail.com>
2026-04-13 11:13:46 +0530

    gallery data json

---
+++ src/data/galleryData.js
@@ -0,0 +1,33 @@
+import classroom from "../assets/gallery/classroom.jpeg";
+import garden from "../assets/gallery/garden.jpeg";
+import playgarden from "../assets/gallery/playgarden.jpeg";
+import playroom from "../assets/gallery/playroom.jpeg";
+import playroom2 from "../assets/gallery/playroom2.jpeg";
+import room1 from "../assets/gallery/room1.jpeg";
+import room2 from "../assets/gallery/room2.jpeg";
+import room3 from "../assets/gallery/room3.jpeg";
+import room4 from "../assets/gallery/room4.jpeg";
+import room5 from "../assets/gallery/room5.jpeg";
+import room6 from "../assets/gallery/room6.jpeg";
+import room7 from "../assets/gallery/room7.jpeg";
+
+
+
+const galleryImages = [
+    { _id: 1, url: classroom},
+    { _id: 2, url: garden },
+    { _id: 3, url: playgarden},
+    { _id: 4, url: playroom},
+    { _id: 5, url: playroom2},
+    { _id: 6, url: room1},
+    { _id: 7, url: room2},
+    { _id: 8, url: room3},
+    { _id: 9, url: room4},
+    { _id: 10, url: room5},
+    { _id: 11, url: room6},
+    { _id: 12, url: room7},
+
+
+];
+
+export default galleryImages;
